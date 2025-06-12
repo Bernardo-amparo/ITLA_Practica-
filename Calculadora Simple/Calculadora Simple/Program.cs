f140713bf66889e26296d9a4c39fd5c4e3cbd210
Bernardo-amparo <bernardoamparo02@gmail.com>
2025-06-11 22:05:21 -0400

    Realizando la actividad de sumar valores

---
+++ Calculadora Simple/Calculadora Simple/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+decimal num1 ;
+decimal num2 ; 
+decimal result ;
+string capturedValue;
+Console.WriteLine("This is the best caculator");
+
+Console.WriteLine("Please Type the first number");
+num1 = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Please Type the secons number");
+num2 = Convert.ToDecimal(Console.ReadLine());
+
+result = num1 + num2;
+
+Console.WriteLine($"The Result of operation is: {result}");
