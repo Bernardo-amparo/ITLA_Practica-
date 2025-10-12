d76aa8e7140206af7de609aec00aca7a86277943
Bernardo-amparo <bernardoamparo02@gmail.com>
2025-10-11 20:59:46 -0400

    Creación de miembros de la comunidad

---
+++ Contact transformed/MiembroDeLaComunidad/MiembroDeLaComunidad/MiembroDeLaComunidad.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiembroDeLaComunidad
+{
+    public abstract class MiembroDeLaComunidad
+    {
+        public string Nombre { get; set; }
+        public int Identificador { get; set; }
+
+        public MiembroDeLaComunidad(string nombre, int id)
+        {
+            Nombre = nombre;
+            Identificador = id;
+        }
+
+        public abstract void Saludar();
+
+        public virtual void MostrarInformacion()
+        {
+            Console.WriteLine($"--- Información del Miembro ---");
+            Console.WriteLine($"Nombre: {Nombre}, ID: {Identificador}");
+        }
+    }
+}
